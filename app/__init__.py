f277b09e4db5b05486a591136b1a65b3e138c490
Jakub Pelec <kuba1pelec@gmail.com>
2021-06-22 10:00:48 +0100

    auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from flask_dance.contrib.github import make_github_blueprint
+import secrets
+import os
 
 app = Flask(__name__)
+
+app.secret_key = secrets.token_hex(16)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+blueprint = make_github_blueprint(
+    client_id="011c8dcc882ac8d13827",
+    client_secret="4fac0e706ba2430a0ef60e5c59b87c38fb382a3b",
+)
+app.register_blueprint(blueprint, url_prefix="/login")
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,8 +1,27 @@
-from flask import render_template
-from app import app
+from flask import render_template, redirect, url_for, session
+from flask_dance.contrib.github import github
+from flask_login import logout_user
+from app import app, blueprint
+
 
 @app.route('/')
+def github_login():
+    if not github.authorized:
+        return redirect(url_for('github.login'))
+    else:
+        account_info = github.get('/user')
+        if account_info.ok:
+            return redirect('/home')
+    return '<h1>Request failed!</h1>'
+
+@app.route('/logout', methods=["POST"])
+def logout():
+    logout_user()
+    return redirect('/')
+
+@app.route('/home')
 def index():
+    print(session)
     return render_template('home.html'), 200
 
 @app.route('/about')
